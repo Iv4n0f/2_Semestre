9a39858afac64c5067772614c743920449b71d3c
Iv4n0f <ivanalexanderlopezzegarra@gmail.com>
2024-01-22 13:19:01 -0500

    jjjjjj

--- CC1/POO/Lab11/complejo.cpp
+++ CC1/POO/Lab11/complejo.cpp
@@ -8,8 +8,8 @@ private:
     int imaginaria;
 
 public:
-    void setReal(int);
-    void setImaginaria(int);
+    Complejo();
+    Complejo(int, int);
     int getReal();
     int getImaginaria();
     void mostrar();
@@ -28,14 +28,16 @@ int Complejo::getImaginaria()
     return imaginaria;
 }
 
-void Complejo::setImaginaria(int i)
+Complejo::Complejo()
 {
-    imaginaria = i;
+    real = 0;
+    imaginaria = 0;
 }
 
-void Complejo::setReal(int r)
+Complejo::Complejo(int r, int i)
 {
     real = r;
+    imaginaria = i;
 }
 
 void Complejo::mostrar()
@@ -49,17 +51,13 @@ void Complejo::mostrar()
 
 Complejo Complejo::suma(Complejo sumando)
 {
-    Complejo suma;
-    suma.setReal(real + sumando.getReal());
-    suma.setImaginaria(imaginaria + sumando.getImaginaria());
+    Complejo suma(real + sumando.getReal(), imaginaria + sumando.getImaginaria());
     return suma;
 }
 
 Complejo Complejo::inverso()
 {
-    Complejo inverso;
-    inverso.setReal(-1 * real);
-    inverso.setImaginaria(-1 * imaginaria);
+    Complejo inverso(real * -1, imaginaria * -1);
     return inverso;
 }
 
@@ -72,21 +70,38 @@ Complejo Complejo::resta(Complejo restando)
 
 int main()
 {
-    Complejo c1, c2;
-    c1.setReal(3);
-    c1.setImaginaria(2);
-
-    c2.setReal(7);
-    c2.setImaginaria(5);
+    Complejo c1(3,9), c2(5,7);
 
     cout << "c1 = ";
     c1.mostrar();
     cout << "c2 = ";
     c2.mostrar();
 
+    //Metodos
     cout << "Suma = ";
     c1.suma(c2).mostrar();
 
     cout <<"Resta = ";
     c1.resta(c2).mostrar();
+
+    //Lista complejos
+    Complejo listaComplejos[10];
+    listaComplejos[0] = c1;
+    listaComplejos[1] = c2;
+
+    for (int i = 0; i < 2; i++)
+        listaComplejos[i].mostrar(); //Metodo en lista
+
+    //Vector con punteros
+
+    Complejo *listaComplejosPunteros[10];
+    listaComplejosPunteros[0] = new Complejo();
+    listaComplejosPunteros[1] = new Complejo(3,8);
+
+    for (int i = 0; i < 2; i++){
+        cout << "Complejo " << i << " = ";
+        listaComplejosPunteros[i]->mostrar(); //Metodo con punteros
+    }
+
+
 }
