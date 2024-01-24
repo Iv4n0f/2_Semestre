cc424f3116d4c25afd3fbb47ea6d8f6b8d06759f
Iv4n0f <ivanalexanderlopezzegarra@gmail.com>
2024-01-24 13:36:41 -0500

    :v

--- CC1/POO/Herencia/HerenciaPersonaAlumno.cpp
+++ CC1/POO/Herencia/HerenciaPersonaAlumno.cpp
@@ -59,8 +59,8 @@ protected:
 public:
     Alumno(string, int, string);
 
-    void setCarrera(string c){carrera=c;}
-    string getCarrera(){return carrera;}
+    void setCarrera(string c) { carrera = c; }
+    string getCarrera() { return carrera; }
 
     void mostrar();
 };
@@ -84,8 +84,8 @@ protected:
 public:
     Profesor(string, int, string);
 
-    void setGrado(string g){grado_acad = g;}
-    string getGrado(){return grado_acad;}
+    void setGrado(string g) { grado_acad = g; }
+    string getGrado() { return grado_acad; }
 
     void mostrar();
 };
@@ -109,13 +109,13 @@ protected:
 public:
     Admin(string, int, string);
 
-    void setCodigo(string c){codigo = c;}
-    string getCodigo(){return codigo;}
+    void setCodigo(string c) { codigo = c; }
+    string getCodigo() { return codigo; }
 
     void mostrar();
 };
 
-Admin::Admin(string n, int e, string c) : Persona(n,e)
+Admin::Admin(string n, int e, string c) : Persona(n, e)
 {
     codigo = c;
 }
@@ -123,18 +123,18 @@ Admin::Admin(string n, int e, string c) : Persona(n,e)
 void Admin::mostrar()
 {
     Persona::mostrar();
-    cout<<"Codigo Aministrativo: "<<codigo<<endl;
+    cout << "Codigo Aministrativo: " << codigo << endl;
 }
 
 int main()
 {
-    Alumno* l_alumnos[100];
+    Alumno *l_alumnos[100];
     int n_alumnos = 0;
 
-    Profesor* l_profesores[100];
+    Profesor *l_profesores[100];
     int n_profesores = 0;
 
-    Admin* l_admins[100];
+    Admin *l_admins[100];
     int n_admins = 0;
 
     string nombre, carrera, grado_acad, codigo;
@@ -142,8 +142,9 @@ int main()
 
     int opc = -1;
 
-    do{
-        //Menu
+    do
+    {
+        // Menu
         cout << "1. Agregar\n";
         cout << "2. Modificar\n";
         cout << "3. Mostrar\n";
@@ -151,150 +152,164 @@ int main()
 
         cin >> opc;
 
-        switch(opc){
+        switch (opc)
+        {
+        case 1:
+            cout << "1. Alumno\n";
+            cout << "2. Profesor\n";
+            cout << "3. Administrador\n";
+            cout << "4. Volver\n";
+            int opc2;
+            cin >> opc2;
+            switch (opc2)
+            {
             case 1:
-                cout << "1. Alumno\n";
-                cout << "2. Profesor\n";
-                cout << "3. Administrador\n";
-                cout << "4. Volver\n";
-                int opc2;
-                cin >> opc2;
-                switch(opc2){
-                    case 1:
-                        cout << "Nombre: ";
-                        cin >> nombre;
-                        cout << "Edad: ";
-                        cin >> edad;
-                        cout << "Carrera: ";
-                        cin >> carrera;
-                        l_alumnos[n_alumnos] = new Alumno(nombre, edad, carrera);
-                        n_alumnos++;
-                        break;
-                    case 2:
-                        cout << "Nombre: ";
-                        cin >> nombre;
-                        cout << "Edad: ";
-                        cin >> edad;
-                        cout << "Grado Academico: ";
-                        cin >> grado_acad;
-                        l_profesores[n_profesores] = new Profesor(nombre, edad, grado_acad);
-                        n_profesores++;
-                        break;
-                    case 3:
-                        cout << "Nombre: ";
-                        cin >> nombre;
-                        cout << "Edad: ";
-                        cin >> edad;
-                        cout << "Codigo Administrativo: ";
-                        cin >> codigo;
-                        l_admins[n_admins] = new Admin(nombre, edad, codigo);
-                        n_admins++;
-                        break;
-                    case 4:
-                        break;
-                    default:
-                        break;
-                }
+                cout << "Nombre: ";
+                cin >> nombre;
+                cout << "Edad: ";
+                cin >> edad;
+                cout << "Carrera: ";
+                cin >> carrera;
+                l_alumnos[n_alumnos] = new Alumno(nombre, edad, carrera);
+                n_alumnos++;
+                break;
             case 2:
-                cout << "1. Alumno\n";
-                cout << "2. Profesor\n";
-                cout << "3. Administrador\n";
-                cout << "4. Volver\n";
-                int opc3;
-                cin >> opc3;
-
-                switch(opc3){
-                    case 1:
-                        cout << "Nombre: ";
-                        cin >> nombre;
-                        cout << "Edad: ";
-                        cin >> edad;
-                        cout << "Carrera: ";
-                        cin >> carrera;
-                        cout << "Numero de alumno: ";
-                        int num;
-                        cin >> num;
-                        num--;
-                        if(num < n_alumnos){
-                            l_alumnos[num]->setNombre(nombre);
-                            l_alumnos[num]->setEdad(edad);
-                            l_alumnos[num]->setCarrera(carrera);
-                            cout << "Modificado" << endl;
-                        }
-                        else{
-                            cout << "No existe ese alumno" << endl;
-                        }
-                        break;
-                    case 2:
-                        cout << "Nombre: ";
-                        cin >> nombre;
-                        cout << "Edad: ";
-                        cin >> edad;
-                        cout << "Grado Academico: ";
-                        cin >> grado_acad;
-                        cout << "Numero de profesor: ";
-                        int num2;
-                        cin >> num2;
-                        num2--;
-                        if(num2 < n_profesores){
-                            l_profesores[num2]->setNombre(nombre);
-                            l_profesores[num2]->setEdad(edad);
-                            l_profesores[num2]->setGrado(grado_acad);
-                        }
-                        else{
-                            cout << "No existe ese profesor" << endl;
-                        }
-                        break;
-                    case 3:
-                        cout << "Nombre: ";
-                        cin >> nombre;
-                        cout << "Edad: ";
-                        cin >> edad;
-                        cout << "Codigo Administrativo: ";
-                        cin >> codigo;
-                        int num3;
-                        cin >> num3;
-                        num3--;
-                        if(num3 < n_admins){
-                            l_admins[num3]->setNombre(nombre);
-                            l_admins[num3]->setEdad(edad);
-                            l_admins[num3]->setCodigo(codigo);
-                            cout << "Modificado" << endl;
-                        }
-                        else{
-                            cout << "No existe ese administrador" << endl;
-                        }
-                        break;
-                    case 4:
-                        break;
-                    default:
-                        break;
-                }
+                cout << "Nombre: ";
+                cin >> nombre;
+                cout << "Edad: ";
+                cin >> edad;
+                cout << "Grado Academico: ";
+                cin >> grado_acad;
+                l_profesores[n_profesores] = new Profesor(nombre, edad, grado_acad);
+                n_profesores++;
+                break;
             case 3:
-                cout << "ALUMNOS: " <<endl;
-                for (int i = 0; i < n_alumnos; i++){
-                    cout << "Alumno " << i+1 << ": ";
-                    l_alumnos[i]->mostrar();
-                }
-                cout << endl;
-
-                cout << "PROFESORES: " <<endl;
-                for (int i = 0; i < n_profesores; i++){
-                    cout << "Profesor " << i+1 << ": ";
-                    l_profesores[i]->mostrar();
-                }
-                cout << endl;
-
-                cout << "ADMINISTRADORES: " <<endl;
-                for (int i = 0; i < n_admins; i++){
-                    cout << "Administrador " << i+1 << ": ";
-                    l_admins[i]->mostrar();
-                }
-                cout << endl;
+                cout << "Nombre: ";
+                cin >> nombre;
+                cout << "Edad: ";
+                cin >> edad;
+                cout << "Codigo Administrativo: ";
+                cin >> codigo;
+                l_admins[n_admins] = new Admin(nombre, edad, codigo);
+                n_admins++;
                 break;
             case 4:
                 break;
+            default:
+                break;
+            }
+            break;
+        case 2:
+            cout << "1. Alumno\n";
+            cout << "2. Profesor\n";
+            cout << "3. Administrador\n";
+            cout << "4. Volver\n";
+            int opc3;
+            cin >> opc3;
+
+            switch (opc3)
+            {
+            case 1:
+                cout << "Numero de alumno: ";
+                int num;
+                cin >> num;
+                num--;
+                if (num < n_alumnos)
+                {
+                    cout << "Nombre: ";
+                    cin >> nombre;
+                    cout << "Edad: ";
+                    cin >> edad;
+                    cout << "Carrera: ";
+                    cin >> carrera;
+                    l_alumnos[num]->setNombre(nombre);
+                    l_alumnos[num]->setEdad(edad);
+                    l_alumnos[num]->setCarrera(carrera);
+                    cout << "Modificado" << endl;
+                }
+                else
+                {
+                    cout << "No existe ese alumno" << endl;
+                }
+                break;
+            case 2:
+                cout << "Numero de profesor: ";
+                int num2;
+                cin >> num2;
+                num2--;
+                if (num2 < n_profesores)
+                {
+                    cout << "Nombre: ";
+                    cin >> nombre;
+                    cout << "Edad: ";
+                    cin >> edad;
+                    cout << "Grado Academico: ";
+                    cin >> grado_acad;
+                    l_profesores[num2]->setNombre(nombre);
+                    l_profesores[num2]->setEdad(edad);
+                    l_profesores[num2]->setGrado(grado_acad);
+                }
+                else
+                {
+                    cout << "No existe ese profesor" << endl;
+                }
+                break;
+            case 3:
+                cout << "Numero de administrador: ";
+                int num3;
+                cin >> num3;
+                num3--;
+                if (num3 < n_admins)
+                {
+                    cout << "Nombre: ";
+                    cin >> nombre;
+                    cout << "Edad: ";
+                    cin >> edad;
+                    cout << "Codigo Administrativo: ";
+                    cin >> codigo;
+                    l_admins[num3]->setNombre(nombre);
+                    l_admins[num3]->setEdad(edad);
+                    l_admins[num3]->setCodigo(codigo);
+                    cout << "Modificado" << endl;
+                }
+                else
+                {
+                    cout << "No existe ese administrador" << endl;
+                }
+                break;
+            case 4:
+                break;
+            default:
+                break;
+            }
+            break;
+        case 3:
+            cout << "ALUMNOS: " << endl;
+            for (int i = 0; i < n_alumnos; i++)
+            {
+                cout << "Alumno " << i + 1 << ": ";
+                l_alumnos[i]->mostrar();
+            }
+            cout << endl;
+
+            cout << "PROFESORES: " << endl;
+            for (int i = 0; i < n_profesores; i++)
+            {
+                cout << "Profesor " << i + 1 << ": ";
+                l_profesores[i]->mostrar();
+            }
+            cout << endl;
+
+            cout << "ADMINISTRADORES: " << endl;
+            for (int i = 0; i < n_admins; i++)
+            {
+                cout << "Administrador " << i + 1 << ": ";
+                l_admins[i]->mostrar();
+            }
+            cout << endl;
+            break;
+        case 4:
+            break;
         }
-    }
-    while(opc != 4);
+    } while (opc != 4);
 }
